e14dcaa02df53753d46e5b996ea92c4c69b1def9
IsuruRajapaksha99 <96106512+IsuruRajapaksha99@users.noreply.github.com>
2022-10-07 23:29:05 +0530

    Add files via upload

--- src/main/java/com/user/LoginUserServ.java
+++ src/main/java/com/user/LoginUserServ.java
@@ -9,7 +9,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.List;
 
-
+@WebServlet
 public class LoginUserServ extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
--- src/main/java/com/user/UserPhoenixDB.java
+++ src/main/java/com/user/UserPhoenixDB.java
@@ -1,11 +1,5 @@
 package com.user;
 
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.Statement;
